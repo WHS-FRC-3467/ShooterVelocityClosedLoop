a9da414aca158e50c0653e52b76a996f1779e0af
FRC_Driver <frc_3467_driver@team3467.org>
2020-02-02 13:18:24 -0500

    added control for second falcon

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -24,7 +24,7 @@
 
 /**
  * Description:
- */
+ */ 
 package frc.robot;
 
 import edu.wpi.first.wpilibj.TimedRobot;
@@ -37,6 +37,7 @@ public class Robot extends TimedRobot {
 
 	/* Hardware */
 	TalonFX m_motor1 = new TalonFX(1);
+	TalonFX m_motor2 = new TalonFX(2);
     
     /* Initialize gains */
 	private double m_kP = 0.25, m_kI  = 0.0, m_kD = 0.0, m_kF = 0.045;
@@ -49,27 +50,40 @@ public class Robot extends TimedRobot {
 		SmartDashboard.putNumber("I", m_kI);
 		SmartDashboard.putNumber("D", m_kD);
 		SmartDashboard.putNumber("Feed Forward", m_kF);
-		SmartDashboard.putNumber("Error", 0);
+		SmartDashboard.putNumber("Error 1", 0);
+		SmartDashboard.putNumber("Error 2", 0);
+
 
         /* Factory Default all hardware to prevent unexpected behaviour */
-      	m_motor1.configFactoryDefault();
-
+		m_motor1.configFactoryDefault();
+		m_motor2.configFactoryDefault();
+		m_motor2.setInverted(true);
 		/* Config sensor used for Primary PID [m_Velocity] */
         m_motor1.configSelectedFeedbackSensor(FeedbackDevice.IntegratedSensor,
                                             Constants.kPIDLoopIdx, 
                                             Constants.kTimeoutMs);
+		m_motor2.configSelectedFeedbackSensor(FeedbackDevice.IntegratedSensor,
+											Constants.kPIDLoopIdx, 
+											Constants.kTimeoutMs);
 
         /**
 		 * Phase sensor accordingly. 
          * Positive Sensor Reading should match Green (blinm_king) Leds on Talon
          */
 		m_motor1.setSensorPhase(false);
+		m_motor2.setSensorPhase(false);
 
+		
 		/* Config the peak and nominal outputs */
 		m_motor1.configNominalOutputForward(0.0, Constants.kTimeoutMs);
 		m_motor1.configNominalOutputReverse(0.0, Constants.kTimeoutMs);
 		m_motor1.configPeakOutputForward(1, Constants.kTimeoutMs);
 		m_motor1.configPeakOutputReverse(0.0, Constants.kTimeoutMs); // Don't go in reverse
+
+		m_motor2.configNominalOutputForward(0.0, Constants.kTimeoutMs);
+		m_motor2.configNominalOutputReverse(0.0, Constants.kTimeoutMs);
+		m_motor2.configPeakOutputForward(1, Constants.kTimeoutMs);
+		m_motor2.configPeakOutputReverse(0.0, Constants.kTimeoutMs); // Don't go in reverse
 	}
 
 	/**
@@ -89,17 +103,28 @@ public class Robot extends TimedRobot {
 		m_motor1.config_kI(Constants.kPIDLoopIdx, kI, Constants.kTimeoutMs);
 		m_motor1.config_kD(Constants.kPIDLoopIdx, kD, Constants.kTimeoutMs);
 	
+		m_motor2.config_kF(Constants.kPIDLoopIdx, kF, Constants.kTimeoutMs);
+		m_motor2.config_kP(Constants.kPIDLoopIdx, kP, Constants.kTimeoutMs);
+		m_motor2.config_kI(Constants.kPIDLoopIdx, kI, Constants.kTimeoutMs);
+		m_motor2.config_kD(Constants.kPIDLoopIdx, kD, Constants.kTimeoutMs);
+	
 		// Convert RPM to raw units per 100ms
 		double targetVelocity_UnitsPer100ms = velocity * 2048 / 600;
 		
 		// Set m_Velocity setpoint
 		m_motor1.set(ControlMode.Velocity, targetVelocity_UnitsPer100ms);
-		
+		m_motor2.set(ControlMode.Velocity, targetVelocity_UnitsPer100ms);
+
 		// Get current speed and convert back to RPM
-		int speed = m_motor1.getSelectedSensorVelocity() * 600 / 2048;
-		SmartDashboard.putNumber("Current Velocity", speed);
+		int speed1 = m_motor1.getSelectedSensorVelocity() * 600 / 2048;
+		SmartDashboard.putNumber("Current Velocity 1", speed1);
+
+		int speed2 = m_motor2.getSelectedSensorVelocity() * 600 / 2048;
+		SmartDashboard.putNumber("Current Velocity 2", speed2);
 
 		// Show the current m_Error on the SDB
-		SmartDashboard.putNumber("Error", m_motor1.getClosedLoopError());
+		SmartDashboard.putNumber("Error 1", m_motor1.getClosedLoopError());
+		SmartDashboard.putNumber("Error 2", m_motor2.getClosedLoopError());
+
 	}
 }
